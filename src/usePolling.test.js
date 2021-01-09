eef724da8ce8d6225e1b685a944419520354690c
Francis Leigh <francis@maisonsport.com>
2021-01-09 10:55:14 +0000

    tests both passing with scenario of one test/

--- src/usePolling.test.js
+++ src/usePolling.test.js
@@ -1,10 +1,9 @@
 import moxios from 'moxios';
 
 import { renderHook, act } from '@testing-library/react-hooks';
-
 import usePolling, { axiosInstance } from './usePolling';
 
-const endpoint = '/test';
+const endpoint = '/test/api';
 const errorText = 'Error!';
 
 const mockPollingResponse = {
@@ -23,149 +22,76 @@ describe('usePolling hook', () => {
     moxios.uninstall();
   });
 
-  function advanceTimer() {
-    jest.useFakeTimers();
-    jest.advanceTimersByTime(1000);
-    jest.useRealTimers();
-  }
-
-  function setupMockEndpoint() {
-    moxios.uninstall();
+  beforeEach(() => {
     moxios.install(axiosInstance);
     moxios.stubRequest(endpoint, mockPollingResponse.pos);
-  }
-
-  test('@prop {func} Should be polled', async () => {
-    const func1 = jest.fn();
-    const func2 = jest.fn();
-    const func3 = jest.fn();
-    const func4 = jest.fn();
-    const func5 = jest.fn();
-
-    const { result, rerender, unmount } = renderHook(() => usePolling({
-      func1,
-      interval: 1000,
-    }));
-
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-    })
-    result.current.stop();
-    expect(func1).toHaveBeenCalledTimes(1);
-    unmount();
-
-    rerender({ func: func2 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-    })
-    result.current.stop();
-    expect(func2).toHaveBeenCalledTimes(2);
-    unmount();
-
-    rerender({ func: func3 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-    })
-    result.current.stop();
-    expect(func3).toHaveBeenCalledTimes(3);
-    unmount();
-
-    rerender({ func: func4 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-    })
-    result.current.stop();
-    expect(func4).toHaveBeenCalledTimes(4);
-    unmount();
-
-    rerender({ func: func5 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-    })
-    result.current.stop();
-    expect(func5).toHaveBeenCalledTimes(5);
-    unmount();
   });
 
-  test('@prop {onResponse} Should be called', async () => {
-    setupMockEndpoint();
-    const onResponse1 = jest.fn();
-    const onResponse2 = jest.fn();
-    const onResponse3 = jest.fn();
-    const onResponse4 = jest.fn();
-    const onResponse5 = jest.fn();
+  beforeAll(() => {
+    jest.setTimeout(30000)
+    jest.useFakeTimers();
+  });
 
-    const { result, unmount, rerender } = renderHook(() => usePolling({
-      endpoint,
-      onResponse1,
-      interval: 1000,
-    }));
+  afterAll(() => {
+    jest.useRealTimers();
+  });
 
-    result.current.start();
-    await act(async () => advanceTimer());
-    result.current.stop();
-    expect(onResponse1).toHaveBeenCalledTimes(1);
-    unmount();
+  test('@prop {func} Should be polled every 1000ms', async () => {
+    const func = jest.fn()
+      .mockImplementationOnce(() => console.log('sync 1'))
+      .mockImplementationOnce(() => console.log('sync 2'))
+      .mockImplementationOnce(() => console.log('sync 3'))
+      .mockImplementationOnce(() => console.log('sync 4'))
+      .mockImplementationOnce(() => console.log('sync 5'))
+      .mockName('func');
 
-    rerender({ onResponse: onResponse2 });
-    result.current.start();
+    const { result } = renderHook(
+      usePolling,
+      {
+        initialProps: {
+          func,
+          interval: 1000,
+        },
+      }
+    );
+
+    expect(func).not.toHaveBeenCalled();
+
     await act(async () => {
-      advanceTimer();
-      advanceTimer();
+      await result.current.start()
     });
-    result.current.stop();
-    expect(onResponse2).toHaveBeenCalledTimes(2);
-    unmount();
+    expect(func).toHaveBeenCalledTimes(1);
 
-    rerender({ onResponse: onResponse3 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-    });
     result.current.stop();
-    expect(onResponse3).toHaveBeenCalledTimes(3);
-    unmount();
+  });
 
-    rerender({ onResponse: onResponse4 });
-    result.current.start();
-    await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-    });
-    result.current.stop();
-    expect(onResponse4).toHaveBeenCalledTimes(4);
-    unmount();
+  test('@prop {onResponse} Should be polled every 1000ms', async () => {
+    const onResponse = jest.fn()
+      .mockImplementationOnce(() => console.log('async 1'))
+      .mockImplementationOnce(() => console.log('async 2'))
+      .mockImplementationOnce(() => console.log('async 3'))
+      .mockImplementationOnce(() => console.log('async 4'))
+      .mockImplementationOnce(() => console.log('async 5'))
+      .mockName('onResponse');
 
-    rerender({ onResponse: onResponse5 });
-    result.current.start();
+    const { result } = renderHook(
+      usePolling,
+      {
+        initialProps: {
+          endpoint,
+          onResponse,
+          interval: 1000,
+        },
+      }
+    );
+
+    expect(onResponse).not.toHaveBeenCalled();
+
     await act(async () => {
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
-      advanceTimer();
+      await result.current.start();
     });
+    expect(onResponse).toHaveBeenCalledTimes(1);
+
     result.current.stop();
-    expect(onResponse5).toHaveBeenCalledTimes(5);
-    unmount();
   });
 });

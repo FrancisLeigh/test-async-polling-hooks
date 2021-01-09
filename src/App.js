19bb71e1f91495d383ee33476a9d41ad19f52c53
Francis Leigh <francis@maisonsport.com>
2021-01-09 10:41:13 +0000

    show how usePolling works in UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
+import { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import usePolling from './usePolling';
+
 function App() {
+  const [view, setView] = useState('sync');
+  const [syncCount, setSyncCount] = useState(0);
+  const [asyncCount, setAsyncCount] = useState(0);
+
+  function incrementSync() {
+    setSyncCount((count) => ++count);
+  }
+  function incrementAsync() {
+    console.log('incrementAsync');
+    setAsyncCount((count) => ++count);
+  }
+
+  const { start: startSync, stop: stopSync } = usePolling({
+    func: incrementSync,
+    interval: 1000,
+  });
+
+  const { start: startAsync, stop: stopAsync } = usePolling({
+    endpoint: 'https://jsonplaceholder.typicode.com/todos/1',
+    onResponse: incrementAsync,
+    interval: 1000,
+  });
+
+  useEffect(() => {
+    startSync();
+    startAsync();
+
+    return () => {
+      stopSync();
+      stopAsync();
+    }
+  } , [view]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>
+            Sync COUNT: {syncCount}
+          </h1>
+          <h1>
+            Async COUNT: {asyncCount}
+          </h1>
       </header>
     </div>
   );
